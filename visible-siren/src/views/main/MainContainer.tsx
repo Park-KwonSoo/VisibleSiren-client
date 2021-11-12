55fe1c0b6c4fdb3a78e867b7e963c8fe36dd6165
ParkKwonsoo <redbean096@khu.ac.kr>
2021-11-12 22:00:09 +0900

    [박권수] feat. MainContainer 작업

--- visible-siren/src/views/main/MainContainer.tsx
+++ visible-siren/src/views/main/MainContainer.tsx
@@ -2,19 +2,44 @@ import React, { useState, useEffect } from 'react';
 
 import MainPresenter from './MainPresenter';
 
+import * as tf from '@tensorflow/tfjs';
+import * as speechCommand from '@tensorflow-models/speech-commands';
 
-const _metadata = '../../config/aiModel/metadata.json';
-const _model = '../../config/aiModel/model.json';
+
+const URL = 'https://teachablemachine.withgoogle.com/models/mEu7EKG82/';
 const _bin = '../../config/aiModel/weights.bin';
 
 const MainContainer = () => {
 
-    const createModel = async() => {
+    const [siren, useSiren] = useState<string>('');
 
+
+    const createModel = async() => {
+        
+        const recognizer = speechCommand.create(
+            "BROWSER_FFT",
+            undefined,
+            URL + 'model.json',
+            URL + 'metadata.json'
+        );
+
+        await recognizer.ensureModelLoaded();
+
+        return recognizer;
+    }
+
+    const fetchData = async () => {
+        const recognizer = createModel();
+        const classLabels = (await recognizer).wordLabels();
+
+        // for (let i = 0; i < classLabels.length; i++) {
+        //     console.log(classLabels[i]);
+        // }
+       
     }
 
     useEffect(() => {
-
+        fetchData();
     }, []);
 
     return (
